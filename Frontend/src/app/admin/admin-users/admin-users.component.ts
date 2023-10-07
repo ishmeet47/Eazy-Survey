8cb6178818cb102964a0a1e4fe76fd9baea5b940
Willie Pai <PaisWillie@gmail.com>
2023-10-07 19:07:58 -0400

    feat: linked add user form

--- Frontend/src/app/admin/admin-users/admin-users.component.ts
+++ Frontend/src/app/admin/admin-users/admin-users.component.ts
@@ -105,4 +105,133 @@ export class AdminUsersComponent implements OnInit {
       }));
     });
   }
+
+  isGroupSelected(groupId: number): boolean {
+    const matchingGroup = this.editingGroups.find((g) => g.id === groupId);
+    return matchingGroup ? !!matchingGroup.selected : false;
+  }
+
+  toggleGroupSelection(groupId: number): void {
+    // First, update the selection status for the editingGroups
+    const editingGroup = this.editingGroups.find((g) => g.id === groupId);
+    if (editingGroup) {
+      editingGroup.selected = !editingGroup.selected;
+    }
+
+    console.log('Updated editing groups:', this.editingGroups);
+  }
+
+  addUser(): void {
+    const userPayload: any = {
+      username: this.newUsername,
+      password: this.newPassword,
+    };
+
+    // Gather the selected group IDs based on checkbox selection from groups list
+    userPayload.groupIds = this.groups
+      .filter((group) => group.selected)
+      .map((group) => group.id);
+
+    this.userService.createUser(userPayload).subscribe(() => {
+      this.newUsername = '';
+      this.newPassword = '';
+      // Reset the group selections in the groups list
+      this.groups.forEach((group) => (group.selected = false));
+      this.loadUsers();
+    });
+  }
+
+  addUserToGroup(): void {
+    // Check if a user is selected
+    if (this.selectedUser === undefined) {
+      console.error('No user selected');
+      return; // or handle this case in a different way
+    }
+
+    const selectedGroups = this.groups
+      .filter((group) => group.selected)
+      .map((group) => group.id);
+
+    // Use a type assertion to assure TypeScript that `this.selectedUser` is a number
+    const payload = {
+      userId: this.selectedUser as number,
+      groupIds: selectedGroups,
+    };
+
+    this.userService.addUserToGroups(payload).subscribe(() => {
+      // You can reset selections and reload data if needed here
+      this.groups.forEach((group) => (group.selected = false));
+    });
+  }
+
+  deleteUser(userId: number | undefined): void {
+    if (typeof userId !== 'number') {
+      console.error('Invalid user ID:', userId);
+      return;
+    }
+
+    this.userService.deleteUser(userId).subscribe(() => {
+      this.loadUsers();
+    });
+  }
+
+  editUser(user: any): void {
+    this.editingUser = { ...user }; // Create a copy of user to avoid direct mutations
+
+    // Clone the groups to editingGroups
+    this.editingGroups = JSON.parse(JSON.stringify(this.groups));
+
+    // Clear any previous selections in the editingGroups list
+    this.editingGroups.forEach((group) => (group.selected = false));
+
+    if (
+      this.editingUser &&
+      this.editingUser.userGroups &&
+      this.editingUser.userGroups.$values
+    ) {
+      // Mark the groups that the user is a part of as selected in the editingGroups list
+      this.editingUser.userGroups.$values.forEach((userGroup) => {
+        const matchingGroup = this.editingGroups.find(
+          (g) => g.id === userGroup.groupId
+        );
+        if (matchingGroup) {
+          matchingGroup.selected = true;
+        }
+      });
+    }
+  }
+
+  updateUser(event: Event): void {
+    event.preventDefault();
+    if (this.editingUser) {
+      const selectedGroups = this.editingGroups
+        .filter((group) => group.selected)
+        .map((group) => group.id);
+      this.editingUser.groupIds = selectedGroups;
+      console.log(this.editingUser.groupIds);
+      this.userService.updateUser(this.editingUser).subscribe(() => {
+        // After updating, you might want to reset the group selections in editingGroups, close the modal, and reload the list of users
+        this.editingGroups.forEach((group) => (group.selected = false));
+        this.editingUser = null; // reset the editing user
+        this.loadUsers();
+      });
+    }
+  }
+
+  onGroupCheckboxChange(event: any, groupId: number): void {
+    if (event.target.checked) {
+      this.selectedGroupIds.push(groupId);
+    } else {
+      const index = this.selectedGroupIds.indexOf(groupId);
+      if (index > -1) {
+        this.selectedGroupIds.splice(index, 1);
+      }
+    }
+  }
+
+  closeEditModal(): void {
+    this.editingUser = null;
+    this.selectedUserGroups = undefined || [];
+    this.editingGroups.forEach((group) => (group.selected = false));
+  }
 }
--- Frontend/src/app/services/user.service.ts
+++ Frontend/src/app/services/user.service.ts
@@ -2,27 +2,30 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { User } from '../modules/user.module';
-import { Group } from '../modules/group.module';  // Assuming you have a Group model
+import { Group } from '../modules/group.module'; // Assuming you have a Group model
 import { map } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
-
   private baseUrl: string = 'http://localhost:5225';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getUsers(): Observable<User[]> {
     return this.http.get<User[]>(`${this.baseUrl}/user/getusers`);
   }
 
-  addUser(username: string, password: string, groupId?: number): Observable<void> {
+  addUser(
+    username: string,
+    password: string,
+    groupId?: number
+  ): Observable<void> {
     const userPayload = {
       username: username,
-      password: password,  // Hash this server-side
-      groupId: groupId
+      password: password, // Hash this server-side
+      groupId: groupId,
     };
     return this.http.post<void>(`${this.baseUrl}/user/adduser`, userPayload);
   }
@@ -33,19 +36,26 @@ export class UserService {
 
   // Update a user's details
   updateUser(user: User): Observable<void> {
-    return this.http.put<void>(`${this.baseUrl}/user/updateuser/${user.id}`, user);
+    return this.http.put<void>(
+      `${this.baseUrl}/user/updateuser/${user.id}`,
+      user
+    );
   }
 
   // Create a new user
-  createUser(payload: { username: string, password: string, groupIds: number[] }): Observable<any> {
+  createUser(payload: {
+    username: string;
+    password: string;
+    groupIds: number[];
+  }): Observable<any> {
     return this.http.post<any>(`${this.baseUrl}/user/create`, payload);
   }
 
-
-
   // Create a new user group
   createGroup(groupName: string): Observable<void> {
-    return this.http.post<void>(`${this.baseUrl}/group/creategroup`, { groupName: groupName });
+    return this.http.post<void>(`${this.baseUrl}/group/creategroup`, {
+      groupName: groupName,
+    });
   }
 
   // getGroups(): Observable<Group[]> {
@@ -53,45 +63,50 @@ export class UserService {
   //   return this.http.get<Group[]>(`${this.baseUrl}/group/getgroups`);
   // }
 
-
   deleteGroupById(groupId: number): Observable<any> {
     const deleteUrl = `${this.baseUrl}/group/deletegroup/${groupId}`; // Adjust the endpoint based on your backend's structure
     return this.http.delete(deleteUrl);
   }
 
-
-
-
   getGroups(): Observable<Group[]> {
-    return this.http.get<{ $id: string; $values: Group[] }>(`${this.baseUrl}/group/getgroups`)
+    return this.http
+      .get<{ $id: string; $values: Group[] }>(`${this.baseUrl}/group/getgroups`)
       .pipe(
-        map(response => response.$values.map(group => {
-          const userGroups = group.userGroups || { $id: "", $values: [] };
-          const surveysAssigned = group.surveysAssigned || { $id: "", $values: [] };
+        map((response) =>
+          response.$values.map((group) => {
+            const userGroups = group.userGroups || { $id: '', $values: [] };
+            const surveysAssigned = group.surveysAssigned || {
+              $id: '',
+              $values: [],
+            };
 
-          return new Group(
-            group.$id,
-            group.id,
-            group.name,
-            {
-              $id: userGroups.$id,
-              $values: userGroups.$values
-            },
-            {
-              $id: surveysAssigned.$id,
-              $values: surveysAssigned.$values
-            },
-            group.lastUpdatedOn,
-            group.lastUpdatedBy
-          );
-        }))
+            return new Group(
+              group.$id,
+              group.id,
+              group.name,
+              {
+                $id: userGroups.$id,
+                $values: userGroups.$values,
+              },
+              {
+                $id: surveysAssigned.$id,
+                $values: surveysAssigned.$values,
+              },
+              group.lastUpdatedOn,
+              group.lastUpdatedBy
+            );
+          })
+        )
       );
   }
 
-
-
-  addUserToGroups(payload: { userId: number, groupIds: number[] }): Observable<void> {
-    return this.http.post<void>(`${this.baseUrl}/user/addusertogroups`, payload);
+  addUserToGroups(payload: {
+    userId: number;
+    groupIds: number[];
+  }): Observable<void> {
+    return this.http.post<void>(
+      `${this.baseUrl}/user/addusertogroups`,
+      payload
+    );
   }
-
 }

f10d39067532a7c5dfdeea1051e8a98d530cd707
Willie Pai <PaisWillie@gmail.com>
2023-10-11 13:34:55 -0400

    feat: added survey page w/ selectable options

--- Frontend/src/app/user/user-survey/user-survey.component.ts
+++ Frontend/src/app/user/user-survey/user-survey.component.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import { IKeyValuePair } from 'src/app/models/IKeyValuePair';
 
 @Component({
   selector: 'app-user-survey',
   templateUrl: './user-survey.component.html',
-  styleUrls: ['./user-survey.component.css']
+  styleUrls: ['./user-survey.component.css'],
 })
 export class UserSurveyComponent implements OnInit {
+  options: IKeyValuePair[] = [
+    {
+      key: 0,
+      value: 'Dental Implants',
+    },
+    {
+      key: 2,
+      value: 'Permanent Retainers',
+    },
+    {
+      key: 3,
+      value: 'False Teeth',
+    },
+    {
+      key: 4,
+      value: 'Veneers',
+    },
+    {
+      key: 5,
+      value: 'None of the above',
+    },
+  ];
 
-  constructor() { }
+  selectedOption: number;
 
-  ngOnInit() {
+  onSelect(option: number) {
+    this.selectedOption = option;
+    console.log('Selected option: ' + this.selectedOption);
   }
 
+  constructor() {
+    this.selectedOption = -1;
+  }
+
+  ngOnInit() {}
 }

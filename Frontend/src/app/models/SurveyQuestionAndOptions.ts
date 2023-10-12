84817cb703c4c6c53631babda69eddc777334aec
Willie Pai <PaisWillie@gmail.com>
2023-10-12 12:26:50 -0400

    feat: display questions and options on survey

---
+++ Frontend/src/app/models/SurveyQuestionAndOptions.ts
@@ -0,0 +1,6 @@
+import { IKeyValuePair } from './IKeyValuePair';
+
+export interface SurveyQuestionAndOptions {
+  question: string;
+  options: IKeyValuePair[];
+}
--- Frontend/src/app/user/user-survey/user-survey.component.ts
+++ Frontend/src/app/user/user-survey/user-survey.component.ts
@@ -1,5 +1,10 @@
-import { Component, OnInit } from '@angular/core';
 import { IKeyValuePair } from 'src/app/models/IKeyValuePair';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { UserAnswerService } from '../../services/userAnswer.service';
+import { new_SAns, new_SOpt, new_SQus } from '../../modules/surveyQ_A.module';
+import { Question } from '../../modules/survey.module';
+import { type } from 'os';
+import { SurveyQuestionAndOptions } from 'src/app/models/SurveyQuestionAndOptions';
 
 @Component({
   selector: 'app-user-survey',
@@ -7,7 +12,7 @@ import { IKeyValuePair } from 'src/app/models/IKeyValuePair';
   styleUrls: ['./user-survey.component.css'],
 })
 export class UserSurveyComponent implements OnInit {
-  options: IKeyValuePair[] = [
+  testOptions: IKeyValuePair[] = [
     {
       key: 0,
       value: 'Dental Implants',
@@ -30,16 +35,161 @@ export class UserSurveyComponent implements OnInit {
     },
   ];
 
+  questions: SurveyQuestionAndOptions[];
+
+  SQList = new Array<new_SQus>();
+  // SQList: new_SQus[] = [];
+  SOptList = new Array<new_SOpt>();
+  SAnsList = new Array<new_SAns>();
+  valid: Boolean = true;
+
+  // Storing current viewing question
+  currentQuestion: string = '';
+  currentOptions: IKeyValuePair[] = [];
+  currentQuestionNumber: number;
+
   selectedOption: number;
+
+  constructor(private UAService: UserAnswerService) {
+    this.selectedOption = -1;
+    this.currentQuestionNumber = 0;
+    this.questions = [];
+  }
+
+  ngOnInit(): void {
+    this.loadQuestionsAndOptions();
+  }
+
+  onContinue(): void {
+    this.currentQuestionNumber++;
+  }
 
   onSelect(option: number) {
     this.selectedOption = option;
     console.log('Selected option: ' + this.selectedOption);
   }
 
-  constructor() {
-    this.selectedOption = -1;
+  // implimnet save draft function
+  onSave(): void {}
+
+  onSubmit(): void {
+    console.log('I CLICKED');
+    console.log('len of SAnsList is ' + this.SAnsList.length);
+
+    // if valid, let user submit
+    this.SAnsList.forEach((ans) => {
+      if (ans.optionId == 0) {
+        console.log("some question didn't answer");
+        this.valid = false;
+      }
+    });
+
+    // checker passed so create survey answer
+    if (this.valid) {
+      this.SAnsList.forEach((ans) => {
+        //define a const
+        const answer: any = {
+          username: ans.userId,
+          password: ans.optionId,
+        };
+        console.log('created');
+        // this.UAService.createAnswer(answer)
+      });
+    }
   }
 
-  ngOnInit() {}
+  // this function updates the SurveyUser DB add current user to the completed database
+  submitSurvey() {
+    const Token: any = {
+      userId: Number(localStorage.getItem('user_id')),
+      surveyId: Number(localStorage.getItem('surveyId')),
+    };
+    this.UAService.submitSurvey(Token);
+  }
+
+  // to update an answer
+  updateAnswer(QuestionId: number, OptId: number) {
+    this.SAnsList.forEach((ans) => {
+      if (ans.QuestionId == QuestionId) {
+        ans.optionId = OptId;
+        console.log('UPDATEEEEEED');
+      }
+    });
+  }
+
+  loadQuestionsAndOptions(): void {
+    this.UAService.getQuestions_new(
+      Number(localStorage.getItem('surveyId'))
+    ).subscribe((data: any) => {
+      //console.log(Response);
+      console.log('response is ++ ');
+      console.log(data.$values);
+      if (data && data.$values) {
+        data.$values.forEach((d: Question) => {
+          const new_ques = new new_SQus(d.id, d.heading, d.surveyId);
+
+          // also init null answers
+          const new_ans = new new_SAns(
+            Number(localStorage.getItem('user_id')),
+            0,
+            d.id
+          );
+          this.SAnsList.push(new_ans);
+
+          const options: IKeyValuePair[] = [];
+
+          // now add all the options inside here
+          this.UAService.getOptions(d.id).subscribe((opt: any) => {
+            if (opt && opt.$values) {
+              opt.$values.forEach((option: new_SOpt) => {
+                const new_opt = new new_SOpt(option.id, option.label, d.id);
+                this.SOptList.push(new_opt);
+
+                options.push({
+                  key: new_opt.id,
+                  value: new_opt.label,
+                });
+              });
+            }
+          });
+
+          this.SQList.push(new_ques);
+
+          this.questions.push({
+            question: new_ques.heading,
+            options: options,
+          });
+        });
+      } else {
+        console.log('sorry no question at this moment');
+      }
+      console.log('now array is');
+      console.log(this.SQList);
+
+      console.log('this.questions: ');
+      console.log(this.questions);
+
+      // Convert to SurveyQuestionAndOptions
+      // this.SQList.forEach((question) => {
+      //   console.log(question.heading);
+      // });
+
+      // console.log('this.SOptList: ');
+      // console.log(this.SOptList);
+
+      // console.log(this.SOptList.length);
+
+      // this.SOptList.forEach((option) => {
+      //   console.log('option: ')
+      //   console.log(option.label);
+      // });
+
+      // console.log('questions: ');
+      // console.log(this.questions);
+    });
+  }
+
+  logout() {
+    localStorage.removeItem('surveyId');
+  }
 }

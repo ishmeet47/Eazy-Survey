987216c2ccccbe6b61d0986f521dd510127e0476
Willie Pai <PaisWillie@gmail.com>
2023-10-15 19:35:18 -0400

    fix: removed admin dashboard page as unnecessary

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { ManageUsersComponent } from './manage-users-component/manage-users-component.component';
-import { AdminDashboardComponent } from './admin/admin-dashboard/admin-dashboard.component';
 import { AdminUsersComponent } from './admin/admin-users/admin-users.component';
 import { AdminSurveysComponent } from './admin/admin-surveys/admin-surveys.component';
 import { AdminGroupsComponent } from './admin/admin-groups/admin-groups.component';
@@ -12,8 +11,7 @@ import { UserSurveyComponent } from './user/user-survey/user-survey.component';
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: 'admin/dashboard', component: AdminDashboardComponent },
-  { path: 'admin/surveys', component: AdminSurveysComponent },
+  { path: 'admin/dashboard', component: AdminSurveysComponent },
   { path: 'admin/groups', component: AdminGroupsComponent },
   { path: 'admin/users', component: AdminUsersComponent },
   { path: 'dashboard', component: UserDashboardComponent },
